1a2aa821ac6c465b39c0366f49f1632db7f471b9
penghaiyang <penghaiyang@wxblockchain.com>
2019-12-30 18:24:41 +0800

    optimize source show

--- src/pages/xirr/index.js
+++ src/pages/xirr/index.js
@@ -1,11 +1,11 @@
-import styles from './index.css';
 import xirr from 'xirr'
-import {Button, Row, Table} from "antd";
-import {columnRender, col, dateFormat, demo, multiply} from "../../utils/table";
+import {Button, notification, Popover, Row, Table} from "antd";
+import {columnRender, col, dateFormat, demo, multiply, days, dayTexts, sourceCol} from "../../utils/table";
 import moment from "moment";
 import numeral from 'numeral';
 import React from "react";
 import lodash from "lodash";
+import styles from './index.css';
 
 export default class XIRR extends React.Component {
   state = {
@@ -15,20 +15,25 @@ export default class XIRR extends React.Component {
   };
 
   componentDidMount() {
-
+    notification.open({
+      message: '提示:',
+      description: '点击Demo按钮可以查看例子',
+    });
   }
 
   calcXIRR = () => {
-    let {source} = this.state;
+    let source =  lodash.cloneDeep(this.state.source);
+    source.map(item=>item.when=moment(item.date).toDate());
+
     const len = source.length - 1;
     let lastAmount = source[len].amount;
-    let lastDate = source[len].when;
+    let lastDate = source[len].date;
     let columns = lodash.cloneDeep(col);
     let dataSource = [];
-    for (let i = 0; i <= 14; i++) {
-      let text = moment(lastDate).add(i, 'd').format(dateFormat);
+    for (let i = 0; i < days.length; i++) {
+      let text = moment(lastDate,dateFormat).add(days[i], 'd').format(dateFormat);
       columns.push({
-        dataIndex: text, key: text, title: text, render: columnRender
+        dataIndex: text, key: text, title: `${text}(${dayTexts[i]})`, render: columnRender
       })
     }
 
@@ -36,8 +41,8 @@ export default class XIRR extends React.Component {
       let text = lastAmount * i;
       source[len].amount = text;
       let obj = {key: i, index: text};
-      for (let i = 0; i <= 14; i++) {
-        let m = moment(lastDate).add(i, 'd');
+      for (let j of days) {
+        let m = moment(lastDate,dateFormat).add(j, 'd');
         let t = m.format(dateFormat);
         source[len].when = m.toDate();
         obj[t] = numeral(xirr(source)).format('0,0.000%');
@@ -47,17 +52,25 @@ export default class XIRR extends React.Component {
     this.setState({columns, dataSource});
   };
   showDemo = () => {
-    this.setState({source: lodash.cloneDeep(demo)}, () => this.calcXIRR())
+    this.setState({source: demo}, () => this.calcXIRR())
   };
 
   render() {
-    const {columns, dataSource} = this.state;
+    const {columns, dataSource,source} = this.state;
+    const content = (
+      <div>
+        <Table columns={sourceCol} dataSource={source} pagination={false} size={'small'} rowKey={'date'}/>
+      </div>
+    );
     return (
       <div className={styles.normal}>
         <Row className={styles.row}>
+          <Popover content={content} title="">
+            <Button type="primary">源数据</Button>
+          </Popover>
           <Button type={'primary'} onClick={this.showDemo}>Demo</Button>
         </Row>
-        <Table columns={columns} dataSource={dataSource} pagination={false} size={'small'}/>
+        <Table columns={columns} dataSource={dataSource} pagination={false} size={'small'} className={styles.table}/>
       </div>
     );
   }
--- src/utils/table.js
+++ src/utils/table.js
@@ -1,10 +1,16 @@
 import React from "react";
 import {Tag} from "antd";
-
+export const sourceCol = [
+  {dataIndex: 'amount', title: '数量', key: 'amount'},
+  {dataIndex: 'date', title: '日期', key: 'date'},
+];
 export const col = [
   {dataIndex: 'index', title: '', key: 'index'}
 ];
-export const dateFormat = 'YYYY-MM-DD';
+export const dateFormat = 'YYYY/M/DD';
+export const days = [0, 1, 2, 3, 4, 5, 6, 7, 14, 21, 28, 60, 90, 180, 270, 360];
+export const dayTexts = ['当天', '1天', '2天', '3天', '4天', '5天', '6天', '1周', '2周', '3周', '1月', '2月', '1季',
+  '2季', '3季', '1年'];
 export const multiply = [0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99, 1, 1.01, 1.02, 1.03, 1.04, 1.05,
   1.06, 1.07, 1.08, 1.09, 1.1];
 export const columnRender = (text, record) => {
@@ -14,9 +20,9 @@ export const columnRender = (text, record) => {
     return text;
   }
 };
-export const demo=  [
-  {amount: -1000, when: new Date(2016, 0, 15)},
-  {amount: -2500, when: new Date(2016, 1, 8)},
-  {amount: -1000, when: new Date(2016, 3, 17)},
-  {amount: 5050, when: new Date(2016, 7, 24)},
+export const demo = [
+  {amount: -1000, date:'2016/1/15'},
+  {amount: -2500, date:'2016/2/8' },
+  {amount: -1000, date:'2016/4/17'},
+  {amount: 5050, date:'2016/8/24'},
 ];
