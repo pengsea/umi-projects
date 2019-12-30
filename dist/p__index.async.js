12b00f3ee935b27289d2f7e43e682d4aee894e77
penghaiyang <penghaiyang@wxblockchain.com>
2019-12-30 19:50:12 +0800

    add html file

---
+++ dist/p__index.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[1],{EDuE:function(n,e,a){n.exports={normal:"normal___1Mxp8"}},RXBc:function(n,e,a){"use strict";a.r(e);var r=a("q1tI"),t=a.n(r),o=a("EDuE"),c=a.n(o);e["default"]=function(){return t.a.createElement("div",{className:c.a.normal},t.a.createElement("a",{href:"xirr.html"},"XIRR"))}}}]);
